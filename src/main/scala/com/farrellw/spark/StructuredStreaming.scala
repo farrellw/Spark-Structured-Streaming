33dc6491a01f55b3e58bde6dc19d340fdac77aab
farrellw <farrell.will.g@gmail.com>
2020-03-10 20:59:31 -0500

    Read words from port on local host. Count unique occurances of words.

---
+++ src/main/scala/com/farrellw/spark/StructuredStreaming.scala
@@ -0,0 +1,40 @@
+package com.farrellw.spark
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+object StructuredStreaming {
+  def main (args: Array[String]): Unit = {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession
+      .builder
+      .appName("StructuredStreaming")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+    val lines = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 9999)
+      .load()
+
+    // Split the lines into words
+    val words = lines.as[String].flatMap(_.split(" "))
+
+    // Generate running word count
+    val wordCounts = words.groupBy("value").count()
+
+    // Start running the query that prints the running counts to the console
+    val query = wordCounts.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+  }
+
+}
